d774152062c313238c974e0679a67a76aeb2b48e
Akwei001 <68909965+Akwei001@users.noreply.github.com>
2022-01-10 09:36:56 +0000

    first commit

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,19 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "omcf7207adb": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "storage": {
+        "s3b6cc3440": {
+            "BucketName": "string",
+            "Region": "string"
+        }
+    }
+}
---
+++ src/script.js
@@ -0,0 +1,68 @@
+import Amplify, { Storage } from 'aws-amplify';
+import awsconfig from './aws-exports';
+
+Amplify.configure(awsconfig);
+
+//Storage.configure({ level: 'public' });
+
+//Display tracks on screen
+
+let tracklist = document.querySelector('.tracklist');
+
+const list = (track) => {
+  Storage.get(track.key).then(() => {
+    //console.log(result);
+    let newListItem = document.createElement('li');
+    newListItem.innerText = track.key;
+    tracklist.appendChild(newListItem);
+  });
+};
+const createAudioPlayer = (track) => {
+  // Get the track from S3
+  Storage.get(track.key).then((result) => {
+    //console.log(track.key);
+    // create an audio element and add a source element to it
+    const audio = document.createElement('audio');
+    const source = document.createElement('source');
+
+    audio.appendChild(source);
+    // add controls to the audio element
+    audio.setAttribute('controls', '');
+    audio.setAttribute('volume', '');
+    // add the track source and type
+    source.setAttribute('src', result);
+    source.setAttribute('type', 'audio/mpeg');
+    // add the item to the page
+    document.querySelector('.tracks').appendChild(audio);
+  });
+};
+
+// select the upload form we created, and listen for a submit event on it
+document.getElementById('upload-form').addEventListener('submit', (e) => {
+  // don't refresh the page on submit
+  e.preventDefault();
+  // get the file from the file upload element, this will be an array.
+  // we only want the first element
+  const file = document.getElementById('file-upload').files[0];
+  // put our file in storage, use the file's name as its S3 Key
+  Storage.put(file.name, file, {
+    progressCallback(progress) {
+      console.log(`Uploaded: ${progress.loaded}/${progress.total}`);
+    },
+  })
+
+    .then((item) => {
+      console.log(item);
+    })
+    .catch((err) => console.error(err));
+});
+
+Storage.list('')
+  .then((result) => {
+    result.forEach((item) => createAudioPlayer(item));
+  })
+  .catch((err) => console.error(err));
+
+Storage.list('').then((result) => {
+  result.forEach((item) => list(item));
+});
