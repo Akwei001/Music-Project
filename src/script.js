e2d541b61ace04adfe4a5a085f560a983c982bea
Akwei001 <68909965+Akwei001@users.noreply.github.com>
2022-04-11 21:48:59 +0100

    audio works with appended controls work on custom player next

--- src/script.js
+++ src/script.js
@@ -3,71 +3,86 @@ import awsconfig from './aws-exports';
 
 Amplify.configure(awsconfig);
 
-const musicContainer = document.getElementById('music-container');
+const musicContainer = document.querySelector('.music-container');
 const playBtn = document.getElementById('play');
 const prevBtn = document.getElementById('prev');
 const nextBtn = document.getElementById('next');
+//const audio = document.createElement('audio');
 
 //Storage.configure({ level: 'public' });
 
-//Get All tracks
-let trackArray = [];
-const getAllTracks = (track) => {
-  // Get the track from S3
+// //Get All tracks
+// let trackArray = [];
+// const getAllTracks = (track) => {
+//   // Get the track from S3
 
-  Storage.get(track.key).then(() => {
-    //console.log(track);
-    trackArray.push(track);
-    // console.log(trackArray);
-    // console.log(newtrackArray);
+//   Storage.get(track.key).then(() => {
+//     //console.log(track);
+//     trackArray.push(track);
+//     // console.log(trackArray);
+//     // console.log(newtrackArray);
 
-    // return newtrackArray;
-  });
-};
+//     // return newtrackArray;
+//   });
+// };
 
-console.log(trackArray);
+// console.log(trackArray);
 
-//Load tracks
-const audio = document.createElement('audio');
-let trackIndex = 1;
+// //Load tracks
+// const audio = document.createElement('audio');
+// const source = document.createElement('source');
+// source.setAttribute('src', 'trackArray.key');
+// source.setAttribute('type', 'audio/mpeg');
 
-function loadTrack(trackIndex) {
-  audio.src = trackArray[trackIndex];
+// let trackIndex = 1;
 
-  audio.load();
-}
+// function loadTrack(trackIndex) {
+//   audio.source = trackArray[trackIndex];
+//   console.log(audio.source);
+//   audio.load();
+// }
 
-loadTrack(trackIndex);
+// loadTrack(trackIndex);
 
-console.log(loadTrack(trackIndex));
+// console.log(loadTrack(trackIndex));
 
 //let Playing_song = false;
 //play Track
 function playTrack() {
+  musicContainer.classList.add('play');
+  playBtn.querySelector('i.fas').classList.remove('fa-play');
+  playBtn.querySelector('i.fas').classList.add('fa-pause');
+
   audio.play();
   // Playing_song = true;
-  play.innerHTML = '<i class="fa fa-pause" aria-hidden="true"></i>';
 }
 
-//pause song
+// //pause song
 // function pauseSong() {
+//   musicContainer.classList.remove('play');
+//   playBtn.querySelector('i.fas').classList.add('fa-play');
+//   playBtn.querySelector('i.fas').classList.remove('fa-pause');
+
 //   audio.pause();
-//   // Playing_song = false;
-//   play.innerHTML = '<i class="fa fa-play" aria-hidden="true"></i>';
 // }
 
-// checking.. the song is playing or not
-// function justplay() {
-//   if (Playing_song == false) {
-//     playTrack();
-//   } else {
-//     pauseSong();
-//   }
-// }
+// // checking.. the song is playing or not
+// // function justplay() {
+// //   if (Playing_song == false) {
+// //     playTrack();
+// //   } else {
+// //     pauseSong();
+// //   }
+// // }
 
-//Event Listeners
+// //Event Listeners
+
+// // let isPlaying = musicContainer.classList.contains('play');
+
+// // let isPlaying = false;
+
 playBtn.addEventListener('click', () => {
-  // const isPlaying = music-container.classList.contains('play');
+  //const isPlaying = musicContainer.classList.contains('play');
   console.log('play clicked');
   playTrack();
 
@@ -82,7 +97,7 @@ playBtn.addEventListener('click', () => {
 
 let tracklist = document.querySelector('.tracklist');
 
-const list = (track) => {
+const List = (track) => {
   Storage.get(track.key).then(() => {
     // console.log(track);
     let newListItem = document.createElement('li');
@@ -91,22 +106,39 @@ const list = (track) => {
   });
 };
 
-// const createAudioPlayer = (track) => {
+const createNewAudio = (track) => {
+  Storage.get(track.key).then((result) => {
+    // console.log(result);
+    console.log(track.key);
+    const audio = document.createElement('audio');
+    const source = document.createElement('source');
+    audio.appendChild(source);
+    //  add the track source and type
+    source.setAttribute('src', result);
+    console.log(track.key);
+    source.setAttribute('type', 'audio/mpeg');
+    audio.setAttribute('controls', '');
+    // add the item to the page
+    document.querySelector('.test').appendChild(audio);
+  });
+};
 
+// const createAudioPlayer = (track) => {
+//   // Get the track from S3
+//   Storage.get(track.key).then((result) => {
 //     // create an audio element and add a source element to it
 //     const audio = document.createElement('audio');
 //     const source = document.createElement('source');
-
 //     audio.appendChild(source);
 //     // add controls to the audio element
-//     audio.setAttribute('controls', '');
-//     audio.setAttribute('volume', '');
+//     // audio.setAttribute('controls', '');
 //     // add the track source and type
 //     source.setAttribute('src', result);
 //     source.setAttribute('type', 'audio/mpeg');
 //     // add the item to the page
-//     document.querySelector('.tracks').appendChild(audio);
-//   }
+//     document.querySelector('.music-container').appendChild(audio);
+//   });
+// };
 
 // select the upload form we created, and listen for a submit event on it
 document.getElementById('upload-form').addEventListener('submit', (e) => {
@@ -128,24 +160,43 @@ document.getElementById('upload-form').addEventListener('submit', (e) => {
     .catch((err) => console.error(err));
 });
 
+//
+
 // Storage.list('')
 //   .then((result) => {
-//     result.forEach((item) => createAudioPlayer(item));
+//     result.forEach((item) => getAllTracks(item));
 //   })
 //   .catch((err) => console.error(err));
 
-Storage.list('')
-  .then((result) => {
-    result.forEach((item) => getAllTracks(item));
-  })
-  .catch((err) => console.error(err));
-
 Storage.list('').then((result) => {
-  result.forEach((item) => list(item));
+  result.forEach((item) => List(item));
 });
 
-// Event listeners
+Storage.list('').then((result) => {
+  createNewAudio(
+    result.map(() => {
+      [];
+    })
+  );
+  console.log(result[1].key);
+});
 
-// function open_list() {
-//   tracklist.classList.toggle('active');
-// }
+console.log(createNewAudio);
+// let Trackresult = [];
+// console.log(Trackresult);
+// // let finalResult = [];
+
+// Storage.list('')
+//   .then((result) => {
+//     List(result);
+//     console.log(result[1].key);
+
+//     Trackresult = result;
+//     console.log(Trackresult[1]);
+//   })
+//   .catch((err) => console.error(err));
+
+// Trackresult = finalResult;
+// console.log(finalResult);
+
+// Event listeners
